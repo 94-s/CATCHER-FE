35fd5e48e4e89226285a2d18290915342d66b3dd
beaever <thebeaever@gmail.com>
2022-08-10 21:24:08 +0900

    에러 수정

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from 'next';
-import { Header } from 'react-uikit-catcher';
-
-const Home: NextPage = () => {
-  return <Header></Header>;
-};
-
-export default Home;
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+import { Header, Icon } from 'react-uikit-catcher';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Header></Header>
+      <Icon name='alert' />
+    </>
+  );
+};
+
+export default Home;
