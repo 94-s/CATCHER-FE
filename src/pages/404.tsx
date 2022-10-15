afa9410a700a5984ee23736b9e6503335c558e98
Jinyoung Kim <76705423+beaever@users.noreply.github.com>
2022-10-15 22:01:08 +0900

    [fix] yarn berry 오류 수정  (#2)
    * git action ci/cd 설계 시작
    
    * 에러 수정
    
    * [feat] emotion module 추가 및 셋팅 (#1)
    
    * [fix] pnp 설정 및 babel 오류 수정
    
    * [fix] sdks, pnpify, stylelint, husky 셋팅
    
    * [fix] husky 설정 오류로 인한 주석처리

---
+++ src/pages/404.tsx
@@ -0,0 +1,13 @@
+/** @jsxImportSource @emotion/react */
+import React from 'react';
+import { css } from '@emotion/react';
+
+const PageNotFound = () => {
+  return (
+    <>
+      <span>404</span>
+    </>
+  );
+};
+
+export default PageNotFound;
